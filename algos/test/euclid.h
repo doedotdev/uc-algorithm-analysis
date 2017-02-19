5ae9c446692bd74ff0a30e4884007760fe27f9da
Benjamin Horn <hornbd96@gmail.com>
2017-02-18 23:01:43 -0500

    Euclid and Euclid test algorithms complete
    Check

--- algos/test/euclid.h
+++ algos/test/euclid.h
@@ -7,7 +7,32 @@
 
 
 class euclid {
+    public:
 
+    // returns the GCD of 2 integers
+
+    long long int getEuclid(long long int a, long long int b){
+        if(a == b){
+            return a;
+        }
+        return gcd(a,b);
+    }
+
+    long long int gcd(long long int a, long long int b){
+        if ( b == 0 ){
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+
+    // checks id gcd returned teh correct gcd of 2 integers
+    bool check(int a, int b){
+        int g = gcd(a,b);
+        return (a%g == 0) && (b%g == 0);
+    }
+
+    private:
+    // NONE??
 };
 
 
--- algos/test/euclidTest.cpp
+++ algos/test/euclidTest.cpp
@@ -12,10 +12,42 @@ using testing::Eq;
 namespace {
     class testClassEuclid : public testing::Test {
     public:
-        euclid object;
+        //
     };
 }
 
 TEST_F(testClassEuclid, test1){
-    ASSERT_EQ(1,1);
+    euclid object;
+
+    ASSERT_EQ(object.getEuclid(0,0),0);
+}
+
+TEST_F(testClassEuclid, test2){
+    euclid object;
+
+    ASSERT_EQ(object.getEuclid(1,1),1);
+}
+
+TEST_F(testClassEuclid, test3){
+    euclid object;
+
+    ASSERT_EQ(object.getEuclid(1234567890123, 1234567890123),1234567890123);
+}
+
+TEST_F(testClassEuclid, test4){
+    euclid object;
+
+    ASSERT_EQ(object.getEuclid(14563,12345626934),1);
+}
+
+TEST_F(testClassEuclid, test5){
+    euclid object;
+
+    ASSERT_EQ(object.getEuclid(2*3*4*7*8*9,982451653),1);
+}
+
+TEST_F(testClassEuclid, test6){
+    euclid object;
+
+    ASSERT_EQ(object.getEuclid(961748941,2*3*4*7*8*9),1);
 }
